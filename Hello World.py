6d46c36a362825f2cf33ca012309e4eb624e4d65
krstn-H <karsten.hans.polim@gmail.com>
2025-09-01 10:35:44 +0700

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,6 +1,7 @@
-// made using pycharm community edition
+# made using pycharm community edition
 name = "Karsten Hans Polim"
 fav_lang = "Python"
 
 print (f"My name is: {name}\nMy favorite programming language is: {fav_lang}")
+
 
